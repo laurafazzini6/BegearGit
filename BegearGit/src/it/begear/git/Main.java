e06a19606cad5956d206e50b39802362b9b8469f
marco <marco@LAPTOP-1JAVPIG4.homenet.telecomitalia.it>
2020-11-19 11:50:46 +0100

    nuovo commit

--- BegearGit/src/it/begear/git/Main.java
+++ BegearGit/src/it/begear/git/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("Marco");
 		
 		Scatola<Cereale> scatola1 = new Scatola<Cereale>(new Cereale());
 		Scatola<Mozzarella> scatola2 = new Scatola<Mozzarella>(new Mozzarella());
